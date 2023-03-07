5984f43ae91726f5b687ce80bec338b758cf2e2b
Ajit <zlksnkwork@gmail.com>
2023-03-07 07:24:24 +0530

    different clipboard manager

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
+	"github.com/atotto/clipboard"
 	"github.com/r3labs/sse/v2"
-	"golang.design/x/clipboard"
 )
 
 func main() {
-	err := clipboard.Init()
-	if err != nil {
-		panic(err)
-	}
-
 	client := sse.NewClient("https://sd.alai-owl.ts.net:1111/events")
-
 	client.Subscribe("text", func(msg *sse.Event) {
-		clipboard.Write(clipboard.FmtText, msg.Data)
+		clipboard.WriteAll(string(msg.Data))
 	})
 }
