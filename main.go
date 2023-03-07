016fde125043a89ceb1d653df848db9b04d7cfed
Ajit <zlksnkwork@gmail.com>
2023-03-07 20:08:28 +0530

    support multiline strings

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	client := sse.NewClient("https://sd.alai-owl.ts.net:1111/events")
+	client.EncodingBase64 = true // if not done, only first line of multiline string will be send, see https://github.com/r3labs/sse/issues/62
 	client.Subscribe("text", func(msg *sse.Event) {
 		clipboard.WriteAll(string(msg.Data))
 	})
