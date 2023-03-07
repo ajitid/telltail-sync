773d6a22c378f3583a885147cc079125dfb290fe
Ajit <zlksnkwork@gmail.com>
2023-03-07 20:50:29 +0530

    auto copy to telltail

--- main.go
+++ main.go
@@ -1,14 +1,73 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
 	"github.com/atotto/clipboard"
 	"github.com/r3labs/sse/v2"
 )
 
-func main() {
-	client := sse.NewClient("https://sd.alai-owl.ts.net:1111/events")
+const Url = "https://sd.alai-owl.ts.net:1111"
+
+func commandExists(cmd string) bool {
+	_, err := exec.LookPath(cmd)
+	return err == nil
+}
+
+func autoSend() {
+	if runtime.GOOS != "linux" {
+		fmt.Println("Your ctrl+c / cmd+c will not be automatically send to telltail as this feature is not supported yet.")
+		return
+	}
+
+	if !commandExists("clipnotify") {
+		fmt.Println("We need `clipnotify` to detect whether if you've copied something. `clipnotify` is only available for X11 systems.")
+		fmt.Println("If you are on X11 put `clipnotify` in any of these paths and rerun this program:", os.Getenv("PATH"))
+		return
+	}
+
+	for {
+		cmd := exec.Command("clipnotify", "-s", "clipboard")
+		_, err := cmd.Output()
+		if err != nil {
+			// this should never happen
+			log.Fatal(err)
+		}
+
+		text, err := clipboard.ReadAll()
+		if err != nil {
+			log.Fatal("clipboard isn't accessible", err)
+		}
+		reader := strings.NewReader(text)
+		http.Post(Url+"/set", "text/plain; charset=UTF-8", reader)
+	}
+}
+
+func autoReceive() {
+	client := sse.NewClient(Url + "/events")
 	client.EncodingBase64 = true // if not done, only first line of multiline string will be send, see https://github.com/r3labs/sse/issues/62
+
 	client.Subscribe("text", func(msg *sse.Event) {
+		clipText, err := clipboard.ReadAll()
+		if err != nil {
+			log.Fatal("clipboard isn't accessible", err)
+		}
+
+		telltailText := string(msg.Data)
+		if clipText == telltailText {
+			return
+		}
 		clipboard.WriteAll(string(msg.Data))
 	})
+}
+
+func main() {
+	go autoSend()
+	autoReceive()
 }
